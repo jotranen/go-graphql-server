7cd3db85216f4fd7982aecc4ac89ad48331f4add
Jari Otranen <jari.otranen@zalando.fi>
2020-03-24 23:22:37 +0200

    #1 initial release

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-
-	"github.com/gin-gonic/gin"
-	"github.com/jotranen/go-graphql-server/internal/handlers"
-)
+import "github.com/jotranen/go-graphql-server/pkg/server"
 
 func main() {
-	host := "localhost"
-	port := "7777"
-	pathQL := "/graphql"
-
-	r := gin.Default()
-
-	r.GET("/ping", handlers.Ping())
-
-	fmt.Println("Running @ http://" + host + ":" + port + pathQL)
-
-	log.Fatalln(r.Run(host + ":" + port))
+	server.Run()
 }
---
+++ pkg/server/main.go
@@ -0,0 +1,30 @@
+package server
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jotranen/go-graphql-server/internal/handlers"
+)
+
+// HOST exported variables
+var HOST string
+// PORT exported variables
+var PORT string
+
+func init() {
+	HOST = "localhost"
+	PORT = "7777"
+}
+
+// Run gin server
+func Run() {
+	r := gin.Default()
+
+	r.GET("/ping", handlers.Ping())
+
+	fmt.Println("Running @ http://" + HOST + ":" + PORT)
+
+	log.Fatalln(r.Run(HOST + ":" + PORT))
+}
